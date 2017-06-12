a37341d7cec1d4c37a54b2b48ab98e5d1cfebbbb
ddutta <ddutta@microsoft.com>
2017-06-12 19:28:36 +0530

    fix

--- server/crawler.py
+++ server/crawler.py
@@ -1,3 +1,22 @@
+ALL_SEEDS = [
+
+{	"categories":"lifestyle",
+	"url":'http://www.anandabazar.com/lifestyle?ref=archive-topnav',
+	"a_xpath1": 'div.row' ,
+	"a_xpath2": 'div.grid-item a',
+	#Note we need 5 entry
+	"data_xpath" : [ 
+		['title','div#story_container','div.storypage_cover_story h1','text','join'],
+		['fullstory','div#story_container','div.articleBody p','text','join'],
+		['images','div#story_container','div.articleBody img','src',None],
+	],
+},
+
+]
+
+
+
+
 from Spider import getData,makeabs
 import pdb
 def process(a):
@@ -9,33 +28,22 @@ def process(a):
         res['main_img'] = res['images'][0]
     return res
 
-url='http://www.anandabazar.com/lifestyle?ref=archive-topnav'
-a_xpath1= 'div.row' 
-a_xpath2= 'div.grid-item a'
-#Note we need 5 entry
-data_xpath =[ 
-            ['title','div#story_container','div.storypage_cover_story h1','text','join'],
-            ['fullstory','div#story_container','div.articleBody p','text','join'],
-            ['images','div#story_container','div.articleBody img','src',None],
-        ]
 
-all = getData(url,a_xpath1,a_xpath2,data_xpath,100);
-all = [process(a) for a in all]
+all = []
+for s in ALL_SEEDS:
+	now = getData(s.get('url'),s.get('a_xpath1'),s.get('a_xpath2'),s.get('data_xpath'),100);
+	all += [process(a) for a in now]
 
 #pdb.set_trace();
 import requests
 URL ='http://52.89.112.230/api/banglagossip2'
 #FIX it remove duplicates...
 for d in all:
-    r = requests.post(URL, json={"url": d['url'],"_cmd":"search","_unicode":"True"})
+	d['_cmd'] = 'create'
+	r = requests.post(URL, json=d)
     #pdb.set_trace()
-    if r.status_code == 200 and len(r.json()['out']) == 0:
-        print 'adding new...'
-        d['_cmd'] ='post'
-        d['_unicode'] ='True'
-        r = requests.post(URL, json=d)
-        print r.json()
-    else:
-        print 'duplicate found...'
-    print 'duplicate found'
-print r.json()
+	if r.status_code == 200:
+		res = r.json();
+		print res['status'],res['msg']
+	else:
+		print 'Internal error..'
